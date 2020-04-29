10222d66c53f47e3c84cdf29dbc38167ce751310
nitinkhatu <nitin.khatu@gmail.com>
2020-04-29 20:55:20 +0530

    Ind-Router updated

--- weather/app.js
+++ weather/app.js
@@ -5,7 +5,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-
+//
+app.use('/', indexRouter);
 var app = express();
 
 // view engine setup
